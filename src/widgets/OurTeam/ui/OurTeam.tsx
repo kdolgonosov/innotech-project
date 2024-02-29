14ff8ebe4d5865038d0848528e3435d820184733
kdolgonosov <89645232+kdolgonosov@users.noreply.github.com>
2024-02-29 20:59:14 +0300

    remove comments

--- src/widgets/OurTeam/ui/OurTeam.tsx
+++ src/widgets/OurTeam/ui/OurTeam.tsx
@@ -8,132 +8,36 @@ import Team5 from '../../../assets/team-5.jpg';
 import Team6 from '../../../assets/team-6.jpg';
 
 export const OurTeam = () => {
-  const images = [Team1, Team2, Team3, Team4, Team5, Team6];
-  return (
-    <section
-      className={styles.ourteam}
-      id='our-team'
-    >
-      <SectionLayout>
-        <h2 className={styles.title}>Our team</h2>
-        <div className={styles.ourteam_decor}></div>
-        <ul className={styles.grid}>
-          {images.map((item, i) => (
-            <li
-              className={styles.grid_item}
-              key={i}
-            >
-              <div className={styles.grid_item_span_container}>
-                <span className={styles.grid_item_item_span_name}>Maxim</span>
-                <span className={styles.grid_item_item_span_post}>
-                  Administrator
-                </span>
-              </div>
-              <img
-                src={item}
-                alt='Team member'
-                className={styles.grid_item_img}
-                width='380'
-                height='400'
-                loading='lazy'
-                decoding='async'
-              />
-            </li>
-          ))}
-          {/* <li className={styles.grid_item}>
-                        <div className={styles.grid_item_span_container}>
-                            <span className={styles.grid_item_item_span_name}>Maxim</span>
-                            <span className={styles.grid_item_item_span_post}>Administrator</span>
-                        </div>
-                        <img
-                            src={Team1}
-                            alt='Team member'
-                            className={styles.grid_item_img}
-                            width='380'
-                            height='400'
-                            loading='lazy'
-                            decoding='async'
-                        />
-                    </li>
-                    <li className={styles.grid_item}>
-                        <div className={styles.grid_item_span_container}>
-                            <span className={styles.grid_item_item_span_name}>Maxim</span>
-                            <span className={styles.grid_item_item_span_post}>Administrator</span>
-                        </div>
-                        <img
-                            src={Team2}
-                            alt='Team member'
-                            className={styles.grid_item_img}
-                            width='380'
-                            height='400'
-                            loading='lazy'
-                            decoding='async'
-                        />
-                    </li>
-                    <li className={styles.grid_item}>
-                        <div className={styles.grid_item_span_container}>
-                            <span className={styles.grid_item_item_span_name}>Maxim</span>
-                            <span className={styles.grid_item_item_span_post}>Administrator</span>
-                        </div>
-                        <img
-                            src={Team3}
-                            alt='Team member'
-                            className={styles.grid_item_img}
-                            width='380'
-                            height='400'
-                            loading='lazy'
-                            decoding='async'
-                        />
-                    </li>
-                    <li className={styles.grid_item}>
-                        <div className={styles.grid_item_span_container}>
-                            <span className={styles.grid_item_item_span_name}>Maxim</span>
-                            <span className={styles.grid_item_item_span_post}>Administrator</span>
-                        </div>
-                        <img
-                            src={Team4}
-                            alt='Team member'
-                            className={styles.grid_item_img}
-                            width='380'
-                            height='400'
-                            loading='lazy'
-                            decoding='async'
-                        />
-                    </li>
-                    <li className={styles.grid_item}>
-                        <div className={styles.grid_item_span_container}>
-                            <span className={styles.grid_item_item_span_name}>Maxim</span>
-                            <span className={styles.grid_item_item_span_post}>Administrator</span>
-                        </div>
-                        <img
-                            src={Team5}
-                            alt='Team member'
-                            className={styles.grid_item_img}
-                            width='380'
-                            height='400'
-                            loading='lazy'
-                            decoding='async'
-                        />
-                    </li>
-                    <li className={styles.grid_item}>
-                        <div className={styles.grid_item_span_container}>
-                            <span className={styles.grid_item_item_span_name}>Maxim</span>
-                            <span className={styles.grid_item_item_span_post}>Administrator</span>
-                        </div>
-                        <img
-                            src={Team6}
-                            alt='Team member'
-                            className={styles.grid_item_img}
-                            width='380'
-                            height='400'
-                            loading='lazy'
-                            decoding='async'
-                        />
-                    </li> */}
-        </ul>
-      </SectionLayout>
-    </section>
-  );
+    const images = [Team1, Team2, Team3, Team4, Team5, Team6];
+    return (
+        <section className={styles.ourteam} id='our-team'>
+            <SectionLayout>
+                <h2 className={styles.title}>Our team</h2>
+                <div className={styles.ourteam_decor}></div>
+                <ul className={styles.grid}>
+                    {images.map((item, i) => (
+                        <li className={styles.grid_item} key={i}>
+                            <div className={styles.grid_item_span_container}>
+                                <span className={styles.grid_item_item_span_name}>Maxim</span>
+                                <span className={styles.grid_item_item_span_post}>
+                                    Administrator
+                                </span>
+                            </div>
+                            <img
+                                src={item}
+                                alt='Team member'
+                                className={styles.grid_item_img}
+                                width='380'
+                                height='400'
+                                loading='lazy'
+                                decoding='async'
+                            />
+                        </li>
+                    ))}
+                </ul>
+            </SectionLayout>
+        </section>
+    );
 };
 
 export default OurTeam;
